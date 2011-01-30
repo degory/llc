9677601d3581f845203e2183f15c16d3e477d385
degs <degs@amity.giantblob.com>
2011-01-30 20:13:56 +0000

    Update lc64.zip

---
+++ lrt-ithunk-linux-x86-64.s
@@ -0,0 +1,36 @@
+
+	##  
+	## %rdi selector id
+	## %rsi object reference
+	##
+	## (object) -> vtable
+	
+	## (vtable) -> super
+	## (vtable+8) -> ?
+	## (vtable+16) -> ?
+	## (vtable+24) -> dispose
+	## (vtable+32) -> interface table
+
+	## itable+0,8	selector id, method address
+	## itable+16,24	selector id, method address
+	## itable+32,40	selector id, method address
+	## ...
+
+	
+	.text
+.globl __ithunk
+	.type	__ithunk, @function
+__ithunk:
+        movq    %rsi, %r10		# object reference
+	movq	(%r10), %r10		# vtable
+	movq	32(%r10), %r10		# selector id -> method mapping table
+
+1:	
+	
+	cmpq	%rdi, (%r10)
+	leaq	16(%r10), %r10
+	jne	1b
+	jmp 	*-8(%r10)
+	
+	.size	__ithunk, .-__ithunk
+
