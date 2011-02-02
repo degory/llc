b85653c9d46364f64dd8ce02cbe1cb57c171dd60
degs <degs@lsp.giantblob.com>
2011-02-02 15:26:56 +0000

    Remove debug message displayed when __geterrno called
    lrt-exception.c:
    * Don't display errno + strerror(errno) in __geterrno()

--- lrt-exception.c
+++ lrt-exception.c
@@ -231,9 +231,11 @@ unsigned char *alloco( int total_size ) {
 }
 
 int __geterrno() {
+  /*
   fprintf( stderr, "errno is: %d\n", errno);
   fprintf( stderr, "error is %s\n", strerror(errno) );
   perror("__geterrno");
+  */
   return errno;
 }
 
