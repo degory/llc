02b9a2e2ca8b97b8cc007f2f74bc05dc5f92574c
degory <degory@dev.giantblob.com>
2017-08-04 19:51:24 +0200

    Clean up build scripts

--- dev-bootstrap.sh
+++ dev-bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v `pwd`:/home/dev/source/ -w /home/dev/source --user dev -it docker.giantblob.com/dev /bin/bash -c "mkdir /tmp/lcache-compiler; pushd jay; make jay; popd; make bootstrap"
+docker run -v `pwd`:/home/dev/source/ -w /home/dev/source --user dev -it docker.giantblob.com/dev /bin/bash -c "./clean.sh && ./bootstrap.sh"
--- dev-build.sh
+++ dev-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v `pwd`:/home/dev/source/ -w /home/dev/source --user dev -it docker.giantblob.com/dev /bin/bash -c "mkdir /tmp/lcache-compiler; pushd jay; make jay; popd; make lc.zip"
+docker run -v `pwd`:/home/dev/source/ -w /home/dev/source --user dev -it docker.giantblob.com/dev /bin/bash -c "./clean.sh && ./build.sh"
