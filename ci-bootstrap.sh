90c9012a945ae28813fa9d5f3e34c363d8d9dd19
degory <degory@dev.giantblob.com>
2017-09-05 20:28:19 +0200

    Docker fixes

--- ci-bootstrap.sh
+++ ci-bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -v /var/lib/jenkins/workspace/:/var/lib/jenkins/workspace/ -w $WORKSPACE --user docker -u `id -u`:`id -g` -t docker.giantblob.com/ex /bin/bash -c "./clean.sh && ./bootstrap.sh"
+docker run -v $WORKSPACE:/home/dev/source -w /home/dev/source -u `id -u`:`id -g` -t docker.giantblob.com/ex /bin/bash -c "./clean.sh && ./bootstrap.sh"
 
 
