4539612bc9c9e2cb19dbf0a12cac0679e85fd385
degory <jeek@giantblob.com>
2017-09-06 11:19:34 +0100

    Convert pretty printer to output ghul

--- dev-build.sh
+++ dev-build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -v `pwd`:/home/dev/source/ -w /home/dev/source --user docker -u `id -u`:`id -g` -it docker.giantblob.com/ex /bin/bash -c "./clean.sh && ./build.sh"
+mkdir -p /tmp/lcache-compiler
+docker run -v /tmp/lcache-compiler:/tmp/lcache-compiler -v `pwd`:/home/dev/source/ -w /home/dev/source --user docker -u `id -u`:`id -g` -it docker.giantblob.com/ex /bin/bash -c "./clean.sh && ./build.sh"
