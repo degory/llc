d546f04af9705f5d263ccf2725d29045c2d1d8ff
degory <jeek@giantblob.com>
2017-09-06 15:59:31 +0100

    Fix print for anon functions and range operator

--- dev-build.sh
+++ dev-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v `pwd`:/home/dev/source/ -w /home/dev/source --user dev -it docker.giantblob.com/dev /bin/bash -c "./clean.sh && ./build.sh"
+docker run -v `pwd`:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` -t docker.giantblob.com/ex /bin/bash -c "./clean.sh && ./build.sh"
