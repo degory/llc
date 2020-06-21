caf56bc39cbfd92b0657fe3ce80e452cb2f18586
degory <degs@giantblob.com>
2020-06-21 10:03:26 +0100

    Rebuild lc.zip with release candidate container for second bootstrap

--- ci-build.sh
+++ ci-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-docker run -v $WORKSPACE:/home/dev/source -w /home/dev/source -u `id -u`:`id -g` ghul/ex:stable /bin/bash -c "./clean.sh && ./build.sh"
+
+if [ -z "$BUILD_WITH" ]; then
+    BUILD_WITH="ghul/ex:stable"
+fi
+
+docker run -v $WORKSPACE:/home/dev/source -w /home/dev/source -u `id -u`:`id -g` $BUILD_WITH /bin/bash -c "./clean.sh && ./build.sh"
 
 
