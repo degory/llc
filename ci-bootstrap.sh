914e785bbf4dfac68f12f2dada59cc2ac972f4bd
degory <degs@giantblob.com>
2020-06-21 09:03:40 +0100

    Bootstrap again with built container on CI build

--- ci-bootstrap.sh
+++ ci-bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-docker run -v $WORKSPACE:/home/dev/source -w /home/dev/source -u `id -u`:`id -g` ghul/ex:stable /bin/bash -c "./clean.sh && ./bootstrap.sh"
+
+if [ -z "$BUILD_WITH" ]; then
+    BUILD_WITH="ghul/ex:stable"
+fi
+
+echo "CI bootstrap using $BUILD_WITH image"
+
+docker run -v $WORKSPACE:/home/dev/source -w /home/dev/source -u `id -u`:`id -g` $BUILD_WITH /bin/bash -c "./clean.sh && ./bootstrap.sh"
 
 
